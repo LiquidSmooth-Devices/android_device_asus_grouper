7eebe2cb14bb210be777876f7a0db360d242af7f
CPA-Poke <brock.shahan@cox.net>
2014-06-07 09:15:54 -0400

    Update build fingerprint

--- liquid.mk
+++ liquid.mk
@@ -33,10 +33,10 @@ $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 # product
 PRODUCT_DEVICE := grouper
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_NAME := liquid_grouper
 PRODUCT_MODEL := Nexus 7
-PRODUCT_MANUFACTURER := Asus
+PRODUCT_MANUFACTURER := asus
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.grouper.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # override
@@ -45,5 +45,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=nakasi \
     TARGET_BUILD_TYPE=user \
     BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys" \
-    BUILD_FINGERPRINT="google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys"
+    PRIVATE_BUILD_DESC="nakasi-user 4.4.3 KTU84L 1148727 release-keys" \
+    BUILD_FINGERPRINT="google/nakasi/grouper:4.4.3/KTU84L/1148727:user/release-keys"
